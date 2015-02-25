9142d4963593e0da2b3276d8fc964d8cc37f5443
Andrew McGill <arm5077@gmail.com>
2015-02-25 17:03:13 -0500

    Changes styles, fixes audio bug

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ app.controller("cpacController", ["$scope", "$http", "$sce", function($scope, $h
 	
 	$scope.renderHTML = function(text){ return $sce.trustAsHtml(text); };
 	
+	$scope.formatTime = function(timestamp){
+		return moment(timestamp).format('MMM. D, h:mm a');
+	};
+	
+	$scope.isNew = function(timestamp){
+		return( ( moment() - moment(timestamp) ) / 1000 / 60 <= 60 );
+	}
+	
+	$scope.playAudio = function(index, quote){
+		sound = document.getElementById(index);
+		quote.activated = true;
+		
+		if( quote.playing ){
+			quote.playing = false;
+			sound.pause();
+			
+		}
+		else {
+			
+			$("audio").each(function(){ this.pause();})
+			$scope.data.categories.forEach(function(category){ category.days.forEach(function(day){  day.quotes.forEach(function(quote){ quote.playing = false }); }); });
+
+			quote.playing = true; 
+			sound.currentTime = 0;
+			sound.play();
+			sound.onended = function() {
+				$scope.$timeout(function(){
+					quote.playing = false;
+				});
+				
+				
+				
+			};
+		}
+	}
+	
 	$http.get("data.json")
 		.error(function(error){
 			console.log("Hit an error! Here it is: " + error);
