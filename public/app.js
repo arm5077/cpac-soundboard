08c79b60c3e23dfa0b6bffa6a9658681ccf10d37
Andrew McGill <arm5077@gmail.com>
2015-02-26 14:55:27 -0500

    Fixes

--- public/app.js
+++ public/app.js
@@ -2,6 +2,12 @@ app = angular.module("cpacApp", []);
 
 app.controller("cpacController", ["$scope", "$http", "$sce", function($scope, $http, $sce){
 	
+	console.log("Hello world!");
+	
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		$scope.mobile = true;
+	}
+	
 	$scope.renderHTML = function(text){ return $sce.trustAsHtml(text); };
 	$scope.trustURL = function(url){ return $sce.trustAsResourceUrl(url) };
 	$scope.prepareForTweet = function(text){
@@ -16,31 +22,48 @@ app.controller("cpacController", ["$scope", "$http", "$sce", function($scope, $h
 		return( ( moment() - moment(timestamp) ) / 1000 / 60 <= 60 );
 	}
 	
+	$scope.turnOffAudio = function(){
+		$("audio").each(function(){ this.pause();})
+		$scope.data.categories.forEach(function(category){ category.days.forEach(function(day){  day.quotes.forEach(function(quote){ quote.playing = false }); }); });
+		console.log("Turning off sound!");
+	}
+	
 	$scope.playAudio = function(index, quote){
+
 		sound = document.getElementById(index);
+		console.log(sound);
 		quote.activated = true;
 		
 		if( quote.playing ){
 			quote.playing = false;
 			sound.pause();
+			console.log("Sound is playing, so I'm turning it off.");
 			
 		}
 		else {
-			
-			$("audio").each(function(){ this.pause();})
-			$scope.data.categories.forEach(function(category){ category.days.forEach(function(day){  day.quotes.forEach(function(quote){ quote.playing = false }); }); });
-
+			$scope.turnOffAudio();
+			console.log("Sound isn't playing, so I'm turning everything else off and turning this on.");
 			quote.playing = true; 
-			sound.currentTime = 0;
-			sound.play();
+			console.log("Ready state is" + sound.readyState);
+			if( sound.readyState == 4){
+				sound.currentTime = 0;
+				sound.play();
+			} else {
+				sound.load();
+				sound.addEventListener("canplay", function(){
+					console.log("Sound loaded!")
+					sound.currentTime = 0;
+					sound.play();
+				});
+			}
+			
 			sound.onended = function() {
-				$scope.$timeout(function(){
+				$scope.$apply(function(){
 					quote.playing = false;
 				});
-				
-				
-				
 			};
+			
+			
 		}
 	}
 	
